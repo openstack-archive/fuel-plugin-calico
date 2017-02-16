70a9d91d5430304f55d001188c08bd7f74874d14
Alexander Arzhanov <aarzhanov@mirantis.com>
2017-02-16 16:17:28 +0300

    Fix puppet manifest to the style guide
    Fix puppet manifest to the style guide
    
    Change-Id: I39d7d2fe0d7a441385bef9e7b61a7312d0b24327

--- deployment_scripts/puppet/manifests/private_gateway_check.pp
+++ deployment_scripts/puppet/manifests/private_gateway_check.pp
@@ -6,7 +6,7 @@ $calico_alt_gateway_br = get_network_role_property('neutron/mesh','interface')
 $calico_alt_gateway    = try_get_value($network_scheme,"endpoints/${calico_alt_gateway_br}/vendor_specific/provider_gateway")
 
 if ! is_ip_address($calico_alt_gateway) {
-  fail("Gateway for Private network does not specified or wrong !!!")
+  fail('Gateway for Private network does not specified or wrong !!!')
 }
 
 # vim: set ts=2 sw=2 et :
--- deployment_scripts/puppet/manifests/repo_setup.pp
+++ deployment_scripts/puppet/manifests/repo_setup.pp
@@ -2,23 +2,27 @@ notice('MODULAR: calico/repo_setup.pp')
 
 # Initial constants
 $plugin_name     = 'fuel-plugin-calico'
-$plugin_settings = hiera_hash("${plugin_name}", {})
+$plugin_settings = hiera_hash($plugin_name, {})
 
 # Bird PPA
 apt::source { 'bird-repo':
-  location    => 'http://ppa.launchpad.net/cz.nic-labs/bird/ubuntu',
-  repos       => 'main',
-  #release     => 'trusty',
-  include     => { 'src' => false },
+  location => 'http://ppa.launchpad.net/cz.nic-labs/bird/ubuntu',
+  repos    => 'main',
+  #release  => 'trusty',
+  include  => {
+    'src' => false,
+  },
 }
 
 # Calico PPA
 apt::source { 'calico-repo':
-  #location    => "http://ppa.launchpad.net/project-calico/stable/ubuntu",
-  location    => "http://ppa.launchpad.net/project-calico/calico-1.4/ubuntu",
-  repos       => 'main',
-  #release     => 'trusty',
-  include     => { 'src' => false },
+  #location => "http://ppa.launchpad.net/project-calico/stable/ubuntu",
+  location => 'http://ppa.launchpad.net/project-calico/calico-1.4/ubuntu',
+  repos    => 'main',
+  #release  => 'trusty',
+  include  => {
+    'src' => false,
+  },
 }
 
 Apt::Source<||> ~> Exec<| title == 'apt_update' |>
